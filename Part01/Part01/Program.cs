e301ba758dc8ae9145335a36293e436379c19abc
Naphorn <naphorn.chok@gmail.com>
2017-11-14 00:17:15 +0700

    Test 2

--- Part01/Part01/Program.cs
+++ Part01/Part01/Program.cs
@@ -6,8 +6,11 @@ namespace ConsoleAppArray
         static void Main(string[] args)
         {
             int[] data = new int[10];
-            data[7] = 7;
-            Console.WriteLine(data[7]);
+            data[4] = 4;
+            data[9] = 9;
+            Console.WriteLine(data[4]);
+            Console.WriteLine(data[9]);
+            Console.WriteLine(data[4] + data[9]);
             Console.ReadLine();
         }
     }
