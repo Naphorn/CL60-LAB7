1b68558d7e7e9cf29fc072834aeee1f0668a1be4
Naphorn <naphorn.chok@gmail.com>
2017-11-14 03:37:22 +0700

    Test. ค้นหาชื่อเดือนที่มีความยาวน้อยที่สุด

--- Part02/Part02/Program.cs
+++ Part02/Part02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleAppArray
 {
@@ -6,34 +7,14 @@ namespace ConsoleAppArray
     {
         static void Main(string[] args)
         {
-            string[] array1 = { "cat", "dog", "carrot", "bird" };
-            //
-            // ค้นหาสมาชิกตัวแรกที่มีค่าตามกำหนด 
-            //
-            string value1 = Array.Find(array1,
-              element => element.StartsWith("car",
-              StringComparison.Ordinal));
+            string[] array1 = { "January", "February", "March", "April",
+                                "May", "June", "July", "August",
+                                "September", "October", "November", "December" };
 
-            string value2 = Array.Find(array1,
-              element => element.StartsWith("fish",
-              StringComparison.Ordinal));
-
-            //
-            // ค้นหาสมาชิกตัวแรกที่มีความยาว string ตามกำหนด
-            //
             string value3 = Array.Find(array1,
-                element => element.Length == 3);
+                element => element.Length <= 3);
 
-            //
-            // ค้นหา string ที่มีความยาวไม่เกินค่าที่กำหนด
-            //
-            string[] array2 = Array.FindAll(array1,
-                element => element.Length <= 4);
-
-            Console.WriteLine(value1);
-            Console.WriteLine(value2);
             Console.WriteLine(value3);
-            Console.WriteLine(string.Join(",", array2));
             Console.ReadLine();
         }
     }
