9f5275b3bd6c4eaab812bc228940e148fdc641b2
Naphorn <naphorn.chok@gmail.com>
2017-11-15 01:42:35 +0700

    การลบสมาชิกและล้างข้อมูลในแฮชเทเบิลด้วยเมธอด Remove() และ Clear()

---
+++ Part24/Part24/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable weeks = new Hashtable();
+            weeks.Add("1", "Sunday");
+            weeks.Add("2", "Monday");
+            weeks.Add("3", "Tuesday");
+            weeks.Add("4", "Wednesday");
+            weeks.Add("5", "Thursday");
+            weeks.Add("6", "Friday");
+            weeks.Add("7", "Saturday");
+
+            Console.WriteLine("---- elements in Hashtable weeks ----");
+            foreach (DictionaryEntry day in weeks)
+            {
+                Console.WriteLine(day.Key + "   -   " + day.Value);
+            }
+
+            Console.WriteLine("\n---- weeks.Remove(\"4\"); ----");
+            weeks.Remove("4");
+            foreach (DictionaryEntry day in weeks)
+            {
+                Console.WriteLine(day.Key + "   -   " + day.Value);
+            }
+
+            Console.WriteLine("\n---- weeks.Clear(); ----");
+            weeks.Clear();
+            foreach (DictionaryEntry day in weeks)
+            {
+                Console.WriteLine(day.Key + "   -   " + day.Value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
