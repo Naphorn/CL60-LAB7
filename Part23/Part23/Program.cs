c8472177ede786eb6d5a0dab5a6dd4fdeb99d76a
Naphorn <naphorn.chok@gmail.com>
2017-11-15 01:39:17 +0700

    การตรวจสอบสมาชิกในแฮชเทเบิลด้วยเมธอด ContainsKey() และ ContainsValue()

---
+++ Part23/Part23/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable weeks = new Hashtable();
+            weeks.Add("1", "Sunday");
+            weeks.Add("2", "Monday");
+            weeks.Add("3", "Tuesday");
+            weeks.Add("4", "Wednesday");
+            weeks.Add("5", "Thursday");
+            weeks.Add("6", "Friday");
+            weeks.Add("7", "Saturday");
+
+            // Method ContainKey()
+            Console.WriteLine("The  key element \"8\" is contain in the Hashtable weeks: " + weeks.ContainsKey("8"));
+
+            // Method ContainValue()
+            Console.WriteLine("The  key element \"Wednesday\" is contain in the Hashtable weeks: " + weeks.ContainsValue("Wednesday"));
+
+            Console.ReadLine();
+        }
+    }
+}
