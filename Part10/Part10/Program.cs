078ea2e2c6b8d47ab6993d4417db5bd64d23c126
Naphorn <naphorn.chok@gmail.com>
2017-11-14 23:45:11 +0700

    Queue การสร้างและใช้งานคิวเบื้องต้น

---
+++ Part10/Part10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+            Console.WriteLine("-- enqueue 3 items -- ");
+            q.Enqueue(10);
+            q.Enqueue(20);
+            q.Enqueue(30);
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
+
+            Console.WriteLine("-- enqueue more items -- ");
+            q.Enqueue(40);
+            q.Enqueue(50);
+
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
+
+            Console.WriteLine("-- dequeue -- ");
+            int i = q.Dequeue();
+            Console.WriteLine("i = {0}", i);
+
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
+            Console.ReadLine();
+        }
+    }
+}
